0705b5618e8841ecdf1dcfe926412ac61a82d38c
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-06 05:00:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var band ={
     heading:'THE BAND',
     content: `We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`
 };
+
 function createTemplate(data) {
   var title=data.title;
   var heading=data.heading;
