f7f64d64d982375c2e1f2a9891c2923d98da1ff7
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-14 01:05:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
-var button=document.getElementById('b1');
-var counter=0;
-
-button.onclick = function() {
-      counter= counter + 1;
-     var span=document.getElementById('count');
-     span.innerHTML= counter.toString();
-};
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" id='madi' class="img-medium"/>
+        </div>
+        <br>
+        <div id='main-text' class="center text-big bold">
+            Hi! This is Priyanshu and this is my first webapp
+            <h2>Hello Everyone</h2>
+        </div>
+        
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+        <br><br>
+        <div class="f1">
+            This button <button id="b1">Click Here!</button>increases counter<span id="count">0</span>times.
+        </div>
+    </body>
+</html>
