b485f386ea77233cabe08473adcd32a85642daee
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-14 00:52:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,13 +8,11 @@ function moveRight() {
     marginLeft+=10;
     img.style.marginLeft=marginLeft+ 'px';
 }
-img.onclick = function() {
-    var interval= setInterval(moveRight,100);
-    
-};
-var counter=0;
+
 var button=document.getElementById('b1');
-button.onclick = function(){
+var counter=0;
+
+button.onclick = function() {
       counter+=1;
      var span=document.getElementById('count');
      span.innerHTML= counter.toString();
