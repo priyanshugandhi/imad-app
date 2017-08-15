cfc9fb8fd297e37fc8d5001887291cc049303cf3
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-15 23:17:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
+
+var counter=0;
+var z=0;
 var element=document.getElementById("madi");
 element.onclick=function(){
- element.style.marginLeft="100px";    
+     for(counter=0;counter<10;counter++)
+     {
+        z+=counter; 
+         element.style.marginLeft=z+"px";
+     }
+     
 };
