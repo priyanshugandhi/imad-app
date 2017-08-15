c73a7334785b54a194d9e7797cc7763ac9b6fee9
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-16 01:35:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,21 +37,32 @@ request.send(null);
 
 
 var submit=document.getElementById('sub');
-
 submit.onclick=function(){
     
-    var nameinput=document.getElementById('na');
-    var name=nameinput.value;
-
-              
+    
+    var request= new XMLHttpRequest();
+    
+    request.onreadystatechange= function(){
+        
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+              var names= request.responseText;
+              names=JSON.parse(names);
               var list= '';
               for(var i=0;i<names.length;i++)
               {
-               
-                 list +='<li>'+  name.toString() + '</li>';
+                 list +='<li>'+ names[i] + '</li>';
               }
               var ul=document.getElementById('namelist');
-              ul.innerHTML=list; 
-   
+              ul.innerHTML=list;  
+            }
+        }
+    };
+  var nameinput=document.getElementById('na');
+  var name=nameinput.value;
+  request.open('GET','http://gandhipriyanshu.imad.hasura-app.io/submit-name?name='+ name);
+  request.send(null);
 };
 
