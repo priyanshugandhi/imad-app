31777262f15ef70f0981142ddbe7adfc7b450b1c
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-16 00:45:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,6 +40,7 @@ var name=nameinput.value;
 var submit=document.getElementById('sub');
 submit.onclick=fucntion(){
   var list= '';
+  var names=['name1','name2','name3'];
   for(var i=0;i<names.length;i++)
   {
       list +='<li>' +names[i] + '</li>';
