ee93539f3fe79d9c821c299429933ad0d48b8377
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-16 01:06:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name', function (req,res) {
+app.get('/:submit-name', function (req,res) {
     
     var name=req.query.name;
     names.push(name);
