07faff880c85a3ff7585096ee95099e3c071a0ed
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-16 01:01:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,15 @@ app.get('/:bandname', function (req,res) {
    res.send(createTemplate(bands[bandname])); 
 });
 
+var names=[];
+app.get('/submit-name', function (req,res) {
+    
+    var name=req.params.name;
+    names.push(name);
+    
+   res.send(JSON.stringify(names)); 
+});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
