3d725173d3868466f25ce17670a7a677794998a7
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-15 23:17:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var counter=0;
 var z=0;
 var element=document.getElementById("madi");
 element.onclick=function(){
-     for(counter=0;counter<10;counter++)
+     for(counter=0;counter>0;counter++)
      {
         z+=counter; 
          element.style.marginLeft=z+"px";
