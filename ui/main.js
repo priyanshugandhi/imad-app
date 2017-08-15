cb8dfe904f62b2bedb253435b3914c3c93c3672e
Priyanshu Gandhi <gandhi.priyanshu@gmail.com>
2017-08-15 23:35:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 
 var counter=0;
+var z=0;
 function Right(){
     counter+=10;
     element.style.marginLeft=counter+"px";
@@ -13,9 +14,9 @@ element.onclick=function(){
 
 var b=document.getElementById("b1");
 b.onclick=function(){
-    counter+=1;
+    z+=1;
     var span=document.getElementById("count");
-    span.innerHTML=counter;
+    span.innerHTML=z;
 };
 
 
